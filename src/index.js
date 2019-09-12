e108a6090bf1d48a643088a7c89ea143106cefb3
Chris Fornesa <17108039+cftxp@users.noreply.github.com>
2019-09-12 15:01:35 -0500

    Import and register the service worker
    This commit will import and register the service worker to allow for offline use of this application.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import Footer from './Footer';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Footer from "./Footer";
+import * as serviceWorker from "./serviceWorker";
 
 // The main application component:
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 ReactDOM.render(<Footer />, document.querySelector("footer"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.register();
