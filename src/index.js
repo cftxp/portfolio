ea3de2f12d14bf11b416903b99946b889b0d972c
Chris Fornesa <admin@cftxp.net>
2019-09-12 20:34:16 -0500

    Redo service worker
    This commit does a redo of the service worker configuration for this application.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import Footer from "./Footer";
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 
 // The main application component:
 ReactDOM.render(<App />, document.getElementById("root"));
@@ -12,4 +12,4 @@ ReactDOM.render(<Footer />, document.querySelector("footer"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
+// serviceWorker.register();
