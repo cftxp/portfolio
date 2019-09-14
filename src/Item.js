f4a2290b119635c8764e270c3115cfaa8bf70293
Christopher Fornesa <admin@cftxp.net>
2019-09-14 13:32:07 -0500

    Adjustments for the semantics and stated hosts
    This commit is for adjusting the semantics for the HTML/JSX tags of each item in the application, as well as acknowledging that different applications are now hosted on different platforms and not just Repl.it.

--- src/Item.js
+++ src/Item.js
@@ -25,48 +25,64 @@ class Item extends React.Component {
     let revealOrNot = this.state.showDescription ? `Hide the case study for the ${this.props.item.title} application.` : `Show the case study for the ${this.props.item.title} application.`;
 
     return <div class="item">
-      {/* Title: */}
-      <h1>{this.props.item.title}</h1>
-      {/* Container class to hold the iframe: */}
-      <div class="image">
-        <Iframe url={this.props.item.site}
-        width="100%" 
-        min-height="550px"
-        height="550px"
-        max-height="550px"
-        scrolling="no"
-        id="myId"
-        className="frame"
-        display="initial"
-        position="relative"
-        allowFullScreen/>
-      </div>
-      {/* A container to hold a description */}
-      <article>
-        <p>{this.props.item.intro}</p>
-      </article>
-      {/* A container to hold the programming skill involved: */}
-      <article>
-        <p>Programming Language/Framework: {this.props.item.skill}.</p>
-      </article>
-      {/* A container to hold the link to the website: */}
-      <article>
-        <p><a href={this.props.item.site} className="App-link" target="_blank" rel="noopener noreferrer">Click here</a> to see the {this.props.item.title} app&rsquo;s full functionalities while <a href={this.props.item.source} className="App-link" target="_blank" rel="noopener noreferrer">this link</a> leads you to the code base of this application (which is hosted on <a href="https://repl.it" className="App-link" target="_blank" rel="noopener noreferrer">Repl.it</a>).</p>
-      </article>
-      <article>
-        <p><span className="reveal-or-not" onClick={() => this.showDetails(this.state.showDescription)}>{revealOrNot}</span></p>
-      </article>
-      <article className={"description " + revealDescription}>
-        <h4>Objective</h4>
-        <ItemComponent content={this.props.item.description.objective} />
-        <p><a href={this.props.item.source} target="_blank" rel="noopener noreferrer">Click here</a> to explore the codebase for this application and <a href={this.props.item.site} target="_blank" rel="noopener noreferrer">click here</a> to see the application itself.</p>
-        <h4>Implementation</h4>
-        <ItemComponent content={this.props.item.description.implementation} />
-        <h4>Features</h4>
-        <ItemComponent content={this.props.item.description.features} />
-        <h4>Lessons Learned</h4>
-        <ItemComponent content={this.props.item.description.learned} />
-      </article>
+      <section>
+        {/* Title: */}
+        <h1>{this.props.item.title}</h1>
+        {/* Container class to hold the iframe: */}
+        <div class="image">
+          <Iframe url={this.props.item.site}
+          width="100%"
+          min-height="550px"
+          height="550px"
+          max-height="550px"
+          scrolling="no"
+          id="myId"
+          className="frame"
+          display="initial"
+          position="relative"
+          allowFullScreen/>
+        </div>
+      </section>
+      <section>
+        {/* A container to hold a description */}
+        <article>
+          <p>{this.props.item.intro}</p>
+        </article>
+        {/* A container to hold the programming skill involved: */}
+        <article>
+          <p>Programming Language/Framework: {this.props.item.skill}.</p>
+        </article>
+        {/* A container to hold the link to the website: */}
+        <article>
+          <p><a href={this.props.item.site} className="App-link" target="_blank" rel="noopener noreferrer">Click here</a> to see the {this.props.item.title} app&rsquo;s full functionalities while <a href={this.props.item.source} className="App-link" target="_blank" rel="noopener noreferrer">this link</a> leads you to the code base of this application (which is hosted on {this.props.item.host}).</p>
+        </article>
+        <article>
+          <p><span className="reveal-or-not" onClick={() => this.showDetails(this.state.showDescription)}>{revealOrNot}</span></p>
+        </article>
+      </section>
+      <section className={"description " + revealDescription}>
+        {/* Set the objective for this application. */}
+        <article>
+          <h4>Objective</h4>
+          <ItemComponent content={this.props.item.description.objective} />
+          <p><a href={this.props.item.source} target="_blank" rel="noopener noreferrer">Click here</a> to explore the codebase for this application and <a href={this.props.item.site} target="_blank" rel="noopener noreferrer">click here</a> to see the application itself.</p>
+        </article>
+          {/* Set the implementation of this application. */}
+        <article>
+          <h4>Implementation</h4>
+          <ItemComponent content={this.props.item.description.implementation} />
+        </article>
+          {/* Set the features of this application. */}
+        <article>
+          <h4>Features</h4>
+          <ItemComponent content={this.props.item.description.features} />
+        </article>
+          {/* Set the lessons learned from building this application. */}
+        <article>
+          <h4>Lessons Learned</h4>
+          <ItemComponent content={this.props.item.description.learned} />
+        </article>
+      </section>
     </div>;
   }
 }
