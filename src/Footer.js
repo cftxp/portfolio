1c6ea10ec70e22e76938af73d03ce5aced64b176
Chris Fornesa <chris@fornesa.co>
2020-01-19 13:27:25 -0600

    Update Footer
    Changes to the footer reflect the change in domain name from chrisfornesa.com to fornesa.design.

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
     let date = new Date();
     let year = date.getFullYear();
 
-    return <p><a href="https://www.chrisfornesa.com" target="_blank" rel="noopener noreferrer">Chris Fornesa</a> &copy; {year} || <a href="https://php.chrisfornesa.com">PHP Portfolio</a> | <a href="https://django.chrisfornesa.com">Django Portfolio</a></p>;
+    return <p><a href="https://www.fornesa.design" target="_blank" rel="noopener noreferrer">Chris Fornesa</a> &copy; {year} || <a href="https://php.fornesa.design">PHP Portfolio</a> | <a href="https://django.fornesa.design">Django Portfolio</a></p>;
   }
 }
 
