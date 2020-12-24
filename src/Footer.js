2e81a57a25d5d83f3a62e3966781402188bbc40a
Chris Fornesa <17108039+cftxp@users.noreply.github.com>
2020-12-24 15:10:04 -0600

    Update Footer.js
    Updated links to reflect the new csf.codes domain name.

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
     let date = new Date();
     let year = date.getFullYear();
 
-    return <p><a href="https://www.fornesa.design" target="_blank" rel="noopener noreferrer">Chris Fornesa</a> &copy; {year} || <a href="https://php.fornesa.design">PHP Portfolio</a> | <a href="https://django.fornesa.design">Django Portfolio</a></p>;
+    return <p><a href="https://www.csf.codes" target="_blank" rel="noopener noreferrer">Chris Fornesa</a> &copy; {year} || <a href="https://php.csf.codes">PHP Portfolio</a> | <a href="https://django.csf.codes">Django Portfolio</a></p>;
   }
 }
 
